71f5cd5b90f8691a45388c87f4ca2204fc1101e3
kolaster123 <carbajalruben1563@gmail.com>
2024-06-26 11:42:36 -0700

    create application schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
+// models/user.js
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +32,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
